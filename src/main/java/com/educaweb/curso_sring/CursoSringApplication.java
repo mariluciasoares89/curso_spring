d248e7f50650757b8978bcfb3fd70584fef61f08
root <mareecleito@gmail.com>
2023-02-23 13:54:50 -0300

    Project created

---
+++ src/main/java/com/educaweb/curso_sring/CursoSringApplication.java
@@ -0,0 +1,13 @@
+package com.educaweb.curso_sring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educaweb/curso_sring/CursoSringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educaweb.curso_sring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
